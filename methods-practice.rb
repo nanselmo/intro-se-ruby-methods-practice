de29b50ba9f65098d69cef993a504d4bb52aabe4
Nicki Anselmo <nickianselmo@gmail.com>
2017-07-24 20:59:57 -0500

    added methods-practice.rb file w/instructions

---
+++ methods-practice.rb
@@ -0,0 +1,22 @@
+#1.Build a method timesByOneThousand that takes an integer and multiplies it by 1000. (We started this one for you)
+#timesByOneThousand(3) should return 3000
+def timesByOneThousand(number)
+      #your code here
+    end
+    
+#Build a method howOldIWillBeIn2030 that takes in your age (as an integer) and tells you how old you will be in the year 2030. 
+#howOldIWillBeIn2020(16) should return 30
+
+#Build a method makeExcited that takes a string, makes it all uppercase and adds three exclamation points. 
+# makeExcited("hello") should return "HELLO!!!"
+
+#Build a method called happyBackwards that takes in a string, reverses it, and then adds on a smiley face character at the end.
+# happyBackwards("cool") should return "looc :)"
+
+#Build a method called isEven that returns a boolean (true or false) depending on whether or not the number is divisible by 2.
+# isEven(3) should return false
+# isEven(4) should return true
+
+#Build a method called gradeCalculator that takes in a number (0 to 100) and returns the letter grade that corresponds to it. 
+#gradeCalculator(95) should return "A"
+#gradeCalculator(85) should return "B"
